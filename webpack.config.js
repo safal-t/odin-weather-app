fd039c77373ccd9a9d0c920fa6d9e87f39537ec8
safal-t <safaltimalsena0@gmail.com>
2025-06-03 11:46:13 +1000

    chore(html): rename html page from template to index

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ export default {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/template.html', // Use your HTML template
+            template: './src/index.html', // Use your HTML template
             filename: 'index.html' // Output filename in dist/
         })
     ],
